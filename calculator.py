55f793208732dab68f50ea4bf98754411ebd5b6f
rudranikashte0-hub <rudranikashte0@gmai.com>
2026-06-29 16:23:35 +0530

    first commit

---
+++ calculator.py
@@ -0,0 +1,26 @@
+def calculate():
+    try:
+        no1=float(input('enter first no. :'))
+        no2=float(input('enter second no. :'))
+        operator=(input('enter(+,-,/,*) :'))
+        if operator=='+':
+            result=no1+no2
+            print(result)
+        elif operator=='-':
+            result=no1-no2
+            print(result)
+        elif operator=='/':
+            result=no1/no2
+            print(result)
+        elif operator=='*':
+            result=no1*no2
+            print(result)
+        else:
+            print('enter proper operation')
+    except:
+        print('zerodivision error') 
+while True:
+    calculate()
+    choice=input('you want to calculate :')
+    if choice.lower()=='no':
+      break
